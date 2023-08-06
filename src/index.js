e272dfb0fb6ceba7dfeaace6559654de21d89e28
Ilavbare Adeola <raisingsrs@gmail.com>
2023-08-06 21:31:55 +0100

    My personal Weather App and Yogurt Landing Page Integrated

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+function submitButton(submitButton){
+    
+    let but = document.querySelector("#progress");
+    but.alert("Coming" + "" + "Soon");
+}
+
+
+let button = document.querySelector("#progress");
+button = addEventListener("click", submitButton);
+
